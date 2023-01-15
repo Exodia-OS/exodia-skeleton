97989096a7ab57ce0e709df083a8b11771543b90
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2023-01-15 03:34:46 +0200

    updated PKGs

--- config/Thunar/accels.scm
+++ config/Thunar/accels.scm
@@ -1,2 +1,3 @@
 (gtk_accel_path "<Actions>/ThunarActions/uca-action-1581593230771688-1" "F4")
 (gtk_accel_path "<Actions>/ThunarActions/uca-action-1622790717174452-1" "F3")
+(gtk_accel_path "<Actions>/ThunarActions/uca-action-1670541881295391-1" "F5")
